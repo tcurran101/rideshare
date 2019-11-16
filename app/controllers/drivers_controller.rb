24b295c55caa9ab89edc6d9a94df1e5324342604
Tiffany Curran <tcurran101@gmail.com>
2019-11-16 16:27:51 +0000

    ???

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -6,4 +6,20 @@ class DriversController < ApplicationController
     def new
         @driver = Driver.new
     end
+
+    def create
+        @driver = drivers.create(driver_params)
+        if @driver.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def driver_params
+      params.require(:driver).permit(:name, :address, :license_no)
+    end
+
 end
